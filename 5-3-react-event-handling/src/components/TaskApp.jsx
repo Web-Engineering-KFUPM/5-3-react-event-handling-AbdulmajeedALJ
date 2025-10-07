5f7318e3db13b6b8e33ab23062b5fd07f90c6981
AbdulmajeedALJ <s202261920@kfupm.edu.sa>
2025-10-07 23:03:11 +0300

    Task 2, Adding the functionality to add tasks

--- 5-3-react-event-handling/src/components/TaskApp.jsx
+++ 5-3-react-event-handling/src/components/TaskApp.jsx
@@ -2,35 +2,37 @@ import React, { useState } from "react";
 import TaskList from "./TaskList";
 
 export default function TaskApp() {
-  const[text, setText] = useState("");
-  
+  const [text, setText] = useState("");
+  const [tasks, setTasks] = useState([]);
+
   const handleSubmit = () => {
-   
+    const trimmedText = text.trim();
+    if (!trimmedText) return;
+
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      { id: Date.now(), text: trimmedText },
+    ]);
+    setText("");
   };
 
-  
   const handleDelete = (id) => {
     // TODO: filter tasks by id to remove the clicked one
   };
 
-  
   const handleClearAll = () => {
     // TODO: set tasks to empty array
   };
 
   return (
     <section className="card">
-      {/*Controlled Input */}
       <div className="inputRow">
         <input
           type="text"
           placeholder="Type a task..."
           className="input"
-          // TODO: value={text}
-          
-          // TODO: onChange={(e) => setText(e.target.value)}
-          onChange= {(e) => setText(e.target.value)}
-
+          value={text}
+          onChange={(e) => setText(e.target.value)}
           onKeyDown={(e) => {
             if (e.key === "Enter") handleSubmit();
           }}
@@ -41,11 +43,8 @@ export default function TaskApp() {
         {text}
       </div>
 
-      {/*Render Task List and Enable Delete */}
-      {/*Pass tasks and onDelete */}
-      <TaskList /* tasks={tasks} onDelete={handleDelete} */ />
+      <TaskList tasks={tasks} />
 
-      {/*Clear All */}
       <div className="footerRow">
         <button className="btn btn--ghost" onClick={handleClearAll}>
           Clear All
--- 5-3-react-event-handling/src/components/TaskList.jsx
+++ 5-3-react-event-handling/src/components/TaskList.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import TaskItem from "./TaskItem";
 
-export default function TaskList({ tasks, onDelete }) {
+export default function TaskList({ tasks }) {
   return (
     <ul className="list">
       {/* Task 2 – Display Placeholder if No Tasks Yet */}
+      {tasks.length === 0 && <li className="list__item">No tasks yet</li>}
 
       {/* Task 2 & 3 – Map tasks to TaskItem */}
-      
+      {tasks.map((task) => (
+        <TaskItem key={task.id} text={task.text} />
+      ))}
     </ul>
   );
 }
